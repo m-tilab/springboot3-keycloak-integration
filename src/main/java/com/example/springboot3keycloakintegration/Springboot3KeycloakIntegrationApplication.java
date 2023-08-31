9aec3ffb2c1855da33434fcee87bc1654cbb3a97
Mahdi Tilab <mahdi.tilab@solvians.com>
2023-08-31 20:35:15 +0200

    add all files

---
+++ src/main/java/com/example/springboot3keycloakintegration/Springboot3KeycloakIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3keycloakintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3KeycloakIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3KeycloakIntegrationApplication.class, args);
+	}
+
+}
